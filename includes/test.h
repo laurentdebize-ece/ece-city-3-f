b30450f5b7a69c9d22a0eb9f8594e64745777b4d
eleuc <eleuchbibi@yahoo.fr>
2022-10-25 15:51:37 +0200

    Tests

---
+++ includes/test.h
@@ -0,0 +1,8 @@
+//
+// Created by eleuc on 25/10/2022.
+//
+
+#ifndef PROJET_TEST_H
+#define PROJET_TEST_H
+
+#endif //PROJET_TEST_H
---
+++ sources/test.c
@@ -0,0 +1,5 @@
+//
+// Created by eleuc on 25/10/2022.
+//
+
+#include "../includes/test.h"
