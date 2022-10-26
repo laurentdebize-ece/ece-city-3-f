256a7295e711e92d2d5b17868679cbebfc8e4082
Charles Masson <charles.masson@edu.ece.fr>
2022-10-26 16:05:12 +0200

    test

--- main.c
+++ main.c
@@ -6,7 +6,17 @@
 #include <raylib.h>
 
 int main (void){
-
+ //open a window using raylib
+    InitWindow(800, 450, "Hello World");
+    SetTargetFPS(60);
+    while (!WindowShouldClose()){
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawText("Hello World", 190, 200, 20, LIGHTGRAY);
+        EndDrawing();
+    }
     printf("Hello World");
     return 0;
 }
+
+
