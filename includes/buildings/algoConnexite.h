b1d29f6afe3d2615259d25382ac02221bacb0b23
theo <theo>
2022-11-09 16:20:43 +0100

    test

---
+++ includes/buildings/algoConnexite.h
@@ -0,0 +1,8 @@
+//
+// Created by theos on 09/11/2022.
+//
+
+#ifndef PROJET_ALGOCONNEXITE_H
+#define PROJET_ALGOCONNEXITE_H
+
+#endif //PROJET_ALGOCONNEXITE_H
---
+++ sources/buildings/algoConnexite.c
@@ -0,0 +1,13 @@
+//
+// Created by theos on 09/11/2022.
+//
+
+#include "../../includes/buildings/algoConnexite.h"
+
+
+void changeConnexite(){
+
+}
+void connexite(){
+
+}
