623a195f73c3624efbef463f4fe6cb14bd7237bb
Charles Masson <charles.masson@edu.ece.fr>
2022-11-25 10:09:47 +0100

    fonction menu encaspulée

--- includes/menu_UI.h
+++ includes/menu_UI.h
@@ -4,11 +4,14 @@
 #include "src/raylib.h"
 
 
+
+
 void bouton_rectangle(Rectangle rec,const char* txt,Color couleur_Base, Color couleur_clique,float font_police_txt,Color couleur_txt);
 void importer_image();
 
 void menu_audio(const char* musique_selec, Music music);
 void menu_sprite(Texture2D image, const char* sprite_a_dessiner);
+void menu();
 
 
 #endif //PROJET_MENU_UI_H
